f74f145a104a9545ffe81f292c888bfd3623f5a7
杉岡成哉 <a1209s518n928@gmail.com>
2023-03-30 19:12:00 +0900

    Initial Commit

---
+++ 100knocks/_00knocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  _00knocksApp.swift
+//  100knocks
+//
+//  Created by 杉岡成哉 on 2023/03/30.
+//
+
+import SwiftUI
+
+@main
+struct _00knocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
