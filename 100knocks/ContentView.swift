6e1c504cb5a177753203be7ad192316a7bdef5d0
杉岡成哉 <a1209s518n928@gmail.com>
2023-03-30 20:55:50 +0900

    完成

--- 100knocks/ContentView.swift
+++ 100knocks/ContentView.swift
@@ -9,20 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .resizable()
-                .scaledToFill()
-                .frame(width: 150, height: 150)
-                .background(Color.red)
-                .clipped()
-                .clipShape(Circle())    // 指定した形状にViewをクリップ
-                .overlay(   // Viewの上にViewを重ねる
-                    RoundedRectangle(cornerRadius: 75)  // 角が丸い長方形
-                        .stroke(Color.blue, lineWidth: 5)   // 輪郭を描画するようにする
-                )
+        HStack {
+            imageView()
+            imageView()
+            imageView()
         }
         .padding()
+    }
+    
+    private func imageView() -> some View {
+        Image(systemName: "globe")
+            .resizable()
+            .scaledToFill()
+            .frame(width: 150, height: 150)
+            .background(Color.red)
+            .clipped()
+            .clipShape(Circle())
     }
 }
 
